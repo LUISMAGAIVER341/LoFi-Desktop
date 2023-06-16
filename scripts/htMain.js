c9678c56cdabb47c7d24204b1d81b6d26ca84624
LUISMAGAIVER341 <84512640+LUISMAGAIVER341@users.noreply.github.com>
2023-06-16 10:07:28 -0300

    implementação do granim.js para construir o wallpaper responsivo

---
+++ scripts/htMain.js
@@ -0,0 +1,14 @@
+var granimInstance = new Granim({
+    element: 'body',
+    direction: 'left-right',
+    isPausedWhenNotInView: false,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#ff9966', '#ff5e62'],
+                ['#00F260', '#0575E6'],
+                ['#e1eec3', '#f05053']
+            ]
+        }
+    }
+});
