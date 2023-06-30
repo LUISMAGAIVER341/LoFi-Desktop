5078b61070a239e8cf5c82b2bb83d2bb50e18154
LUISMAGAIVER341 <84512640+LUISMAGAIVER341@users.noreply.github.com>
2023-06-30 16:33:10 -0300

    asdasdasdasd

--- scripts/htMain.js
+++ scripts/htMain.js
@@ -1,14 +1,2 @@
-var granimInstance = new Granim({
-    element: '"OmG',
-    direction: 'left-right',
-    isPausedWhenNotInView: false,
-    states : {
-        "default-state": {
-            gradients: [
-                ['#ff9966', '#ff5e62'],
-                ['#00F260', '#0575E6'],
-                ['#e1eec3', '#f05053']
-            ]
-        }
-    }
-});
+var dock = getElementById(dock);
+alert(dock.styles.color);
